4e2c68fc3e687daf034ad9d3ab99fee80cf010a3
David Mills <dave@1morepx.com>
2018-03-02 13:32:00 -0500

    final result

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const sassMiddleware = require("node-sass-middleware");
 const path = require("path");
 
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(
+  sassMiddleware({
+    src: path.join(__dirname, "styles"),
+    dest: path.join(__dirname, "public", "styles"),
+    debug: true,
+    outputStyle: "compressed",
+    prefix: "/public/styles"
+  })
+);
+
 app.use("/public", express.static(path.join(__dirname, "public")));
 
 let murrays = [
